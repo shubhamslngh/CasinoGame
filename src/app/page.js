c3816b60ebd224d7a1242786397ec0f09fe64c38
shubhamsingh851 <shus@americana-food.com>
2024-10-20 11:14:28 +0530

    mx

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import GameCarousel from "./gamesCarousel";
 
 export default function Home() {
   return (
-    <div class="grid grid-rows-4 space-x-8 mx-auto grid-flow-col gap-6 px-4">
+    <div className="grid grid-rows-4 space-x-8 grid-flow-col gap-6 max-w-full">
       {/* Viking Image */}
       <div className="scale-110 row-span-4 w-[15vw] h-[72vh] hidden md:block">
         <Image
@@ -13,16 +13,16 @@ export default function Home() {
           className="object-cover"
         />
       </div>
-        {/* First Carousel */}
-        <div className="row-span-2 w-100 h-100 justify-center sm:h-[50] sm:w-[50] py-2 overflow-y-hidden overflow-x-hidden">
-          <GameCarousel />
-        </div>
 
-        {/* Second Carousel */}
-        <div className=" row-span-2 w-100 h-100 justify-center sm:w-[50] py-2 overflow-y-hidden overflow-x-hidden">
-          <GameCarousel />
-        </div>
-    
+      {/* First Carousel */}
+      <div className="row-span-2 w-full h-100 justify-center sm:h-[50] sm:w-[50] py-2 overflow-y-hidden overflow-x-hidden">
+        <GameCarousel />
+      </div>
+
+      {/* Second Carousel */}
+      <div className="row-span-2 w-full h-100 justify-center sm:h-[50] sm:w-[50] py-2 overflow-y-hidden overflow-x-hidden">
+        <GameCarousel />
+      </div>
     </div>
   );
 }
